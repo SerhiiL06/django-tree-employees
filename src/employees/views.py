72f49a2be2b18c12ed5ef48c2936858aa468eb15
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2024-04-20 00:10:11 +0300

    completed imlemented searching and filtering

--- src/employees/views.py
+++ src/employees/views.py
@@ -1,8 +1,6 @@
 from typing import Any
-from django.shortcuts import render
 from django.db.models.query import QuerySet
 from django.db.models import Q
-from django.http import HttpRequest, HttpResponse
 from .models import Employee
 from django.views.generic import ListView
 
@@ -22,7 +20,17 @@ class EmployeeListView(ListView):
 
     def get_queryset(self) -> QuerySet[Any]:
         queryset = super().get_queryset()
+
         if self.request.GET.get("search"):
             search_text = self.request.GET.get("search")
-            queryset = queryset.filter(first_name__icontains=search_text)
+            queryset = queryset.filter(
+                Q(first_name__icontains=search_text)
+                | Q(last_name__icontains=search_text)
+                | Q(middle_name__icontains=search_text)
+            )
+
+        if self.request.GET.get("order"):
+            order_fields = self.request.GET.get("order")
+            queryset = queryset.order_by(order_fields)
+
         return queryset
