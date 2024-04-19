1215e8434e10eeed2507e3c3ddafb7c88112c734
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2024-04-19 23:09:53 +0300

    implemented searh functionality

--- src/employees/views.py
+++ src/employees/views.py
@@ -1,5 +1,8 @@
 from typing import Any
+from django.shortcuts import render
 from django.db.models.query import QuerySet
+from django.db.models import Q
+from django.http import HttpRequest, HttpResponse
 from .models import Employee
 from django.views.generic import ListView
 
@@ -16,3 +19,10 @@ class EmployeeListView(ListView):
     template_name = "list.html"
     model = Employee
     paginate_by = 50
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        if self.request.GET.get("search"):
+            search_text = self.request.GET.get("search")
+            queryset = queryset.filter(first_name__icontains=search_text)
+        return queryset
